db6ba599e4228e683adde44630c4de9ecd34f226
luizfrodrigues <luizfernando.rodrigues@outlook.com>
2021-10-01 22:27:38 -0300

    :tada: Exercicio 1010 e pasta de entrada

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 var input = require('fs').readFileSync('stdin', 'utf-8');
+var lines = input.split('\n');
+
+let [_idItem1, quantidadeItem1, valorUnitarioItem1] = lines.shift().split(' ');
+let [_idItem2, quantidadeItem2, valorUnitarioItem2] = lines.shift().split(' ');
+
+let [valorTotalItem1, valorTotalItem2] = [
+  quantidadeItem1 * valorUnitarioItem1,
+  quantidadeItem2 * valorUnitarioItem2,
+];
+
+valorTotal = (valorTotalItem1 + valorTotalItem2).toFixed(2);
+
+console.log(`VALOR A PAGAR: R$ ${valorTotal}`);
